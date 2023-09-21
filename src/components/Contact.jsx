c7320301152b21bda9ae5474ed373f3ee1f4d66b
overflowDev <adeyemiadetomiwa.dev@gmail.com>
2023-09-21 11:33:34 +0100

    getlinked challenge

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,20 +1,46 @@
-
+import { Link } from 'react-router-dom'
+import { useState, useEffect } from "react"
 
 import Star from '../assets/star.svg'
 import StarPu from '../assets/starpu.svg'
 import Navbar from '../layouts/Navbar'
 
 function Contact() {
+
+    const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 768); // Adjust the breakpoint as needed
+
+    useEffect(() => {
+        function handleResize() {
+        setIsDesktop(window.innerWidth >= 768); // Adjust the breakpoint as needed
+        }
+
+        window.addEventListener("resize", handleResize);
+        return () => {
+        window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
   return (
     <div className='overflow-hidden h-screen '>
-        <Navbar />
+        {isDesktop ? (
+            <Navbar /> 
+        ) : (
+            <nav className='py-6 md:px-24 px-8 flex justify-start'>
+                <Link to='/' className='md:hidden flex items-center justify-center w-8 h-8 rounded-full bg-gradient-to-r from-lgrad to-secondary'>
+                    <div className='w-7 h-7 rounded-full flex items-center justify-center bg-main'>
+                        <ion-icon name="chevron-back-outline"></ion-icon>
+                    </div>
+                </Link>
+            </nav>
+        )}
+
         <div className='md:px-24 px-8 h-full flex items-center relative'>
             <div className="absolute md:top-36 md:left-36 top-24 right-24 w-96 h-96 bg-primary rounded-full blur-3xl opacity-20 "></div>
             <div className="absolute -bottom-24 -right-4 md:w-96 w-72 md:h-96 h-72 bg-primary rounded-full blur-3xl opacity-20 "></div>
             <img src={Star} alt="star" className='absolute md:top-[20%] md:left-[20%] w-3 animate-pulse' />
             <img src={Star} alt="star" className='absolute md:bottom-[20%] md:right-[50%] bottom-[47%] right-[10%] w-4 animate-pulse' />
 
-            <div className='md:flex md:justify-between md:items-center items-start md:py-12 py-8 w-full'>
+            <div className='md:flex md:justify-between md:items-center items-start md:py-12 py-8 w-full h-full'>
                 
                 <div className='hidden md:flex flex-col max-w-xl md:mt-0 mt-12 gap-4'>
                     <div className='md:flex md:justify-start justify-center hidden'>
@@ -45,10 +71,10 @@ function Contact() {
                     </div>
                 </div>
 
-                <div className='lg:w-6/12 h-full'>
-                    <div className='p-12 md:w-3/4 md:h-full h-full md:rounded-md md:shadow-lg md:shadow-black md:bg-white md:backdrop-blur md:bg-clip-padding md:backdrop-filter md:bg-opacity-5'>
-                        <h2 className='font-clash-display text-secondary w-64 text-lg font-bold '>Question or need assistance? Let us know about it!</h2>
-
+                <div className='lg:w-6/12 md:h-max h-full'>
+                    <div className='md:p-12 p-4 md:w-3/4 md:h-full h-full md:rounded-md md:shadow-lg md:shadow-black md:bg-white md:backdrop-blur md:bg-clip-padding md:backdrop-filter md:bg-opacity-5'>
+                        <h2 className='font-clash-display text-secondary md:w-64 w-full md:text-lg text-xl font-bold '>Question or need assistance? Let us know about it!</h2>
+                        <h3 className='block md:hidden mb-12'>Email us below to any question related to our event</h3>
                         <div className='flex-col '>
                             <form>
                                 <input 
@@ -57,22 +83,38 @@ function Contact() {
                                     placeholder='First Name'
                                 />
                                 <input 
-                                    type="text" 
-                                    className='w-full border px-3 rounded-md bg-secondary bg-opacity-5 backdrop-blur mt-8 py-2 text-white placeholder:text-white focus:text-white focus:outline-none'
-                                    placeholder='First Name'
+                                    type="email" 
+                                    className='w-full border px-3 rounded-md bg-secondary bg-opacity-5 backdrop-blur md:mt-6 mt-8 py-2 text-white placeholder:text-white focus:text-white focus:outline-none'
+                                    placeholder='Email'
+                                />
+                                <input 
+                                    type="number" 
+                                    className='w-full border px-3 rounded-md bg-secondary bg-opacity-5 backdrop-blur md:mt-6 mt-8 py-2 text-white placeholder:text-white focus:text-white focus:outline-none'
+                                    placeholder='Phone Number'
                                 />
                                 <textarea 
-                                    className='resize-none w-full border px-3 rounded-md bg-secondary bg-opacity-5 backdrop-blur mt-8 py-2 text-white placeholder:text-white focus:text-white focus:outline-none'
+                                    className='resize-none w-full border px-3 rounded-md bg-secondary bg-opacity-5 backdrop-blur md:mt-6 mt-8 py-2 text-white placeholder:text-white focus:text-white focus:outline-none'
                                     name="message" 
                                     cols="5" 
                                     rows="3"
                                     placeholder='Message'
                                 ></textarea>
 
-                                <div className='flex items-center mt-6'>
-                                    <button type='submit' className='bg-gradient-to-r from-lgrad to-grad py-2 px-8 md:ml-36 rounded-sm'>Submit</button>
+                                <div className='flex justify-center mx-auto mt-6'>
+                                    <button type='submit' className='bg-gradient-to-r from-lgrad to-grad py-2 px-8 rounded-sm'>Submit</button>
                                 </div>
                             </form>
+
+                            <div className='md:hidden flex justify-center items-center flex-col mt-12'>
+                                <h2 className="text-secondary text-sm font-bold">Follow us</h2>
+                                <div className='flex items-center space-x-2 mt-2'>
+                                    <ion-icon name="logo-instagram"></ion-icon>
+                                    <ion-icon name="logo-twitter"></ion-icon>
+                                    <ion-icon name="logo-facebook"></ion-icon>
+                                    <ion-icon name="logo-linkedin"></ion-icon>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
